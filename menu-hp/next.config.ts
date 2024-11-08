1a182d1ca78bd8660d22094c62ac145b2d6c63b6
K3-Y <blue.dolphin.1827@gmail.com>
2024-11-08 23:22:48 +0900

    元に戻した

--- menu-hp/next.config.ts
+++ menu-hp/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  imges: {
-    unoptimized: true,
-  }
 };
 
 export default nextConfig;
